dbff4dca206c94698ea5973a9fd22d8e90d7a9f3
Alan Jachimczak <alanj1998@MacBook-Pro-Alan.local>
2018-09-13 22:17:11 +0100

    removed source code from master

--- src/bin/start.ts
+++
@@ -1,9 +0,0 @@
-import { Server } from '../server'
-
-let app = new Server(3001)
-
-try {
-    app.start()
-} catch (error) {
-    console.log(error)
-}
--- src/controllers/index.ts
+++
@@ -1,7 +0,0 @@
-import { HelloController as Hello } from './hello'
-import { ByeController as Bye } from './bye'
-
-export default [
-    new Hello(),
-    new Bye()
-]
