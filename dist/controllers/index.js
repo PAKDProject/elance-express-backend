7891ef82ca07f0f061fc9eceb4a062a1029d270b
Killian Kelly <Killiank360@gmail.com>
2018-09-17 21:49:49 +0100

    Undid mistake

---
+++ dist/controllers/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var hello_1 = require("./hello");
+var bye_1 = require("./bye");
+exports.default = [
+    new hello_1.HelloController(),
+    new bye_1.ByeController()
+];
+//# sourceMappingURL=index.js.map
