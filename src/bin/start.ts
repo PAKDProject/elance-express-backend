dab5017d3a176aa0182fc811a7b0b2bc1be28b64
Alan Jachimczak <alanj1998@MacBook-Pro-Alan.local>
2018-09-20 21:45:45 +0100

    added checks to see if server is configured!

--- src/bin/start.ts
+++ src/bin/start.ts
@@ -5,7 +5,7 @@ import { error, info } from '../../helpers/logger'
 (async () => {
     try {
         let app = new Server(3001)
-        await app.config()
+        //await app.config()
         app.start()
     } catch (err) {
         error(err)
