903e2a2954430d32c428593c5552fc588824eaab
Alan Jachimczak <alanj1998@MacBook-Pro-Alan.local>
2018-09-20 21:49:16 +0100

    Removed forgotten comment

--- src/bin/start.ts
+++ src/bin/start.ts
@@ -4,7 +4,7 @@ import { error } from '../../helpers/logger'
 (async () => {
     try {
         let app = new Server(3001)
-        //await app.config()
+        await app.config()
         app.start()
     } catch (err) {
         error(err)
