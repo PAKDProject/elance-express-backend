57d40304fe19b69bbc7014aef47e7d8dfe0c7dd9
Alan Jachimczak <alanj1998@MacBook-Pro-Alan.local>
2018-09-20 21:23:28 +0100

    added command friendly logger

---
+++ helpers/logger.ts
@@ -0,0 +1,6 @@
+import * as colors from "colors/safe";
+
+export let error = (message: string) => { console.log(colors.red(message)) }
+export let warning = (message: string) => { console.log(colors.yellow(message)) }
+export let info =  (message: string) => { console.log(colors.white(message)) }
+export let success =  (message: string) => { console.log(colors.green(message)) }
