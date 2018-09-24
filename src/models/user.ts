ce399357eb7b0e580ec04ac028dff8944e8e48b6
Killian Kelly <Killiank360@gmail.com>
2018-09-24 17:21:49 +0100

    Added properties to user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,17 +1,107 @@
-import { Typegoose, prop, ModelType, staticMethod } from 'Typegoose'
+import { Typegoose, prop, ModelType, staticMethod, arrayProp, Ref } from 'Typegoose'
 import { ObjectId } from 'mongodb';
+import { Job } from './job';
 
 /**
 * Model class for User
 * @extends Typegoose
 */
-export class User extends Typegoose {
+
+export class Skill {
+    @prop()
+    title?: string
+
+    @prop()
+    description?: string
+}
+
+export class EducationItem {
+    @prop()
+    degreeTitle?: string
+
+    @prop()
+    startYear?: Date
+
+    @prop()
+    endYear?: Date
+
+    @prop()
+    collegeName?: string
+
+    @prop()
+    grade?: string
+
+    @prop()
+    description?: string
+}
+
+export class SocialLink {
     @prop()
     name?: string
 
-    constructor(name?: string) {
+    @prop()
+    linkUrl?: string
+}
+
+export class User extends Typegoose {
+    @prop()
+    username?: string
+
+    @prop()
+    fName?: string
+
+    @prop()
+    lName?: string
+
+    @prop()
+    dob?: Date
+
+    @prop()
+    summary?: string
+
+    @arrayProp({items: Skill})
+    skills?: Skill[]
+
+    @arrayProp({items: EducationItem})
+    educationItems?: EducationItem[]
+
+    @arrayProp({itemsRef: Job})
+    activeJobs?: Ref<Job>[]
+
+    @arrayProp({itemsRef: Job})
+    jobHistory?: Ref<Job>[]
+
+    @prop()
+    avatarUrl?: string
+
+    @prop()
+    backgroundUrl?: string
+
+    @arrayProp({items: SocialLink})
+    socialLinks?: SocialLink[]
+
+    @prop()
+    tagline?: string
+
+    @arrayProp({itemsRef: User})
+    contacts?: User[]
+
+    constructor(username?: string, fName?: string, lName?: string, dob?: Date, summary?: string, skills?: Skill[], educationItems?: EducationItem[], activeJobs?: Job[], jobHistory?: Job[], avatarUrl?: string, backgroundUrl?: string, socialLinks?: SocialLink[], tagline?: string, contacts?: User[]) {
         super()
-        this.name = name
+        this.username = username
+        this.fName = fName
+        this.lName = lName
+        this.dob = dob
+        this.summary = summary
+        this.skills = skills
+        this.educationItems = educationItems
+        this.activeJobs = activeJobs
+        this.jobHistory = jobHistory
+        this.avatarUrl = avatarUrl
+        this.backgroundUrl = backgroundUrl
+        this.socialLinks = socialLinks
+        this.tagline = tagline
+        this.contacts = contacts
     }
     /**
     * Default method for finding all Users
