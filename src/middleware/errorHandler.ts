f26e02d2ad72895a2db39805cd3b8a826fa5961a
Alan Jachimczak <alanj1998@MacBook-Pro-Alan.local>
2018-09-24 19:33:47 +0100

    added comments

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,7 +1,13 @@
 import { Request, Response, NextFunction } from "express";
-import { error } from '../helpers/logger'
 const environment = process.env.NODE_ENV
 
+/**
+ * Error handler which spits out error to the console. If environment is in development, the whole stack trace is shown.
+ * @param err 
+ * @param req 
+ * @param res 
+ * @param next 
+ */
 export let errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
     if (environment == 'production') {
         res.send(err.name).status(500)
--- src/middleware/errorLogger.ts
+++ src/middleware/errorLogger.ts
@@ -1,6 +1,13 @@
 import { Request, Response, NextFunction } from "express";
 import { error } from '../helpers/logger'
 
+/**
+ * Error logger which logs errors into the console and passes it to another error handler
+ * @param err 
+ * @param req 
+ * @param res 
+ * @param next 
+ */
 export let errorLogger = (err: Error, req: Request, res: Response, next: NextFunction) => {
     error(`initiated at: ${req.url}\n${err.message} : ${err.stack}`)
     next(err)
