08492d1f14c34f4dfa65b539a69504fde27d78dc
Killian Kelly <killiank360@gmail.com>
2018-09-25 22:10:57 +0100

    finished insert user test, now using chai http

--- src/tests/controllers/usercontroller.spec.ts
+++ src/tests/controllers/usercontroller.spec.ts
@@ -17,22 +17,57 @@ describe('Testing the User Controller', () => {
     })
 
     it('Should return user at /:id when id is passed', done => {
-        fetch(apiUrl + '')
-        .then(users => {
-            chai.expect(users).to.not.be.null
-            chai.expect(users.status).to.equal(200)
-            chai.expect(users).to.not.be.undefined
+        chai.request(apiUrl)
+        .get('')
+        .end((_err, res) => {
+            chai.expect(res).to.not.be.null
+            chai.expect(res.status).to.equal(200)
+            chai.expect(res).to.not.be.undefined
             done()
         })
     })
 
     it('Should insert a new user at /', done => {
+        const user = {
+            username: 'xXx_JeffBezos_xXx',
+            fName: 'Jeff',
+            lName: 'Bezos',
+            dob: new Date('01/01/01'),
+            summary: 'Ayy its ya boi, skinnybezos',
+            skills: [
+                {
+                    title: 'God King of Earth',
+                    description: 'I have too much money'
+                },
+                {
+                    title: 'Absolutely Shredded',
+                    description: 'gains gains gains'
+                }
+            ],
+            educationItems: [
+                {
+                    degreeTitle: 'PhD in Being Rich',
+                    startYear: '2017',
+                    endYear: '2999',
+                    collegeName: 'AWS Institute',
+                    grade: '69',
+                    description: 'Why is this a field loool 4Head'
+                }
+            ],
+            avatarUrl: 'bezos.png',
+            backoundUrl: 'background.png',
+            socialLinks: [
+                {
+                    name: 'Github',
+                    linkUrl: 'https://github.com/BlueishLeaf'
+                }
+            ],
+            tagline: 'The real OG'
+        }
         chai.request(apiUrl)
         .post('/')
-        .send({
-            username: 'xXxJeffBezosxXx'
-        })
-        .then(res => {
+        .send(user)
+        .end((_err, res) => {
             chai.expect(res.status).to.equal(201)
             done()
         })
