fa506472d28a0576780f99a0f1b84b6b11303056
Alan Jachimczak <alanj1998@MacBook-Pro-Alan.local>
2018-09-25 14:19:27 +0100

    deleted test controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,10 +1,8 @@
-import { TestController as Test } from "./testCon"
 import { UserController as User } from "./userCon"
 import { LoginController as Login } from "./loginCon"
 
 // All backend routes listed below
 export default [
-    new Test(),
     new User(),
     new Login()
 ]
