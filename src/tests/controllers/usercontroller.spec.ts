82ce1d6ac4af341ac3280bfca9b0d57cc4b4ab7e
Killian Kelly <killiank360@gmail.com>
2018-09-30 14:15:16 +0100

    [ci skip] cleaner testing, fixed some bugs

--- src/tests/controllers/usercontroller.spec.ts
+++ src/tests/controllers/usercontroller.spec.ts
@@ -2,6 +2,7 @@ import { describe, before, it, beforeEach } from "mocha"
 import * as chai from 'chai'
 import { Server } from '../../lib/createServer'
 import chaiHttp = require('chai-http')
+import { UserModel } from "../../models/user";
 
 chai.use(chaiHttp)
 
@@ -11,12 +12,80 @@ describe('Testing the User Controller', () => {
         let app = new Server( undefined, 'development')
         app.config().then(() => { 
             app.start()
+            new UserModel({
+                email: 'jeffmoneybezos1@aws.com',
+                fName: 'Jeff',
+                lName: 'Bezos',
+                dob: new Date('01/01/01'),
+                summary: 'Ayy its ya boi, skinnybezos',
+                skills: [
+                    {
+                        title: 'God King of Earth',
+                        description: 'I have too much money'
+                    },
+                    {
+                        title: 'Absolutely Shredded',
+                        description: 'gains gains gains'
+                    }
+                ],
+                educationItems: [
+                    {
+                        degreeTitle: 'PhD in Being Rich',
+                        startYear: '2017',
+                        endYear: '2999',
+                        collegeName: 'AWS Institute',
+                        grade: '69',
+                        description: 'Why is this a field loool 4Head'
+                    }
+                ],
+                avatarUrl: 'bezos.png',
+                backoundUrl: 'background.png',
+                socialLinks: [
+                    {
+                        name: 'Github',
+                        linkUrl: 'https://github.com/BlueishLeaf'
+                    }
+                ],
+                tagline: 'The real OG'
+            }).save()
+            new UserModel({
+                email: 'jeffmoneybezos2@aws.com',
+                fName: 'Jeff',
+                lName: 'Bezos',
+                dob: new Date('01/01/01'),
+                summary: 'Ayy its ya boi, skinnybezos',
+                skills: [
+                    {
+                        title: 'God King of Earth',
+                        description: 'I have too much money'
+                    },
+                    {
+                        title: 'Absolutely Shredded',
+                        description: 'gains gains gains'
+                    }
+                ],
+                educationItems: [
+                    {
+                        degreeTitle: 'PhD in Being Rich',
+                        startYear: '2017',
+                        endYear: '2999',
+                        collegeName: 'AWS Institute',
+                        grade: '69',
+                        description: 'Why is this a field loool 4Head'
+                    }
+                ],
+                avatarUrl: 'bezos.png',
+                backoundUrl: 'background.png',
+                socialLinks: [
+                    {
+                        name: 'Github',
+                        linkUrl: 'https://github.com/BlueishLeaf'
+                    }
+                ],
+                tagline: 'The real OG'
+            }).save()
             done()
         })
-    })
-
-    beforeEach((done) => {
-        
     })
 
     it('Should return all users', done => {
@@ -25,6 +94,7 @@ describe('Testing the User Controller', () => {
         .end((_err, res) => {
             chai.expect(res).to.not.be.null
             chai.expect(res.status).to.equal(200)
+            chai.expect(res.body).to.have.property('users')
             chai.expect(res).to.not.be.undefined
             done()
         })
@@ -32,8 +102,7 @@ describe('Testing the User Controller', () => {
 
     it('Should insert a new user at /', done => {
         const user = {
-            username: 'xXx_JeffBezos_xXx',
-            email: 'jeffmoneybezor@aws.com',
+            email: 'jeffmoneybezos3@aws.com',
             fName: 'Jeff',
             lName: 'Bezos',
             dob: new Date('01/01/01'),
@@ -83,15 +152,19 @@ describe('Testing the User Controller', () => {
             fName: 'Money',
             lName: 'Bags'
         }
-        chai.request(apiUrl)
-        .put('/' + '5baab09a480dbc1757b7f010')
-        .send(userChanges)
-        .end((_err, res) => {
-            chai.expect(res.status).to.equal(202)
-            chai.expect(res.body).to.have.property('msg').eql('User updated.')
-            chai.expect(res.body.user).to.have.property('fName').eql('Money')
-            chai.expect(res.body.user).to.have.property('lName').eql('Bags')
-            done()
+        UserModel.findOne({}, (_err, user) => {
+            if(user){
+                chai.request(apiUrl)
+                .put('/' + user._id)
+                .send(userChanges)
+                .end((_err, res) => {
+                    chai.expect(res.status).to.equal(202)
+                    chai.expect(res.body).to.have.property('msg').eql('User updated.')
+                    chai.expect(res.body.user).to.have.property('fName').eql('Money')
+                    chai.expect(res.body.user).to.have.property('lName').eql('Bags')
+                    done()
+                })
+            }
         })
     })
     
@@ -99,7 +172,10 @@ describe('Testing the User Controller', () => {
 
     it('Should delete the user at /:id when ID is passed')
 
-    after(() => {
-        process.exit(0)
+    after(done => {
+        UserModel.deleteMany({}, (_err) => {
+            done()
+            process.exit(0)
+        })
     })
 })
