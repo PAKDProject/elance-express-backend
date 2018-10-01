4e002f81dfc58c938233f76a61c9da814bd8538c
Alan Jachimczak <alanj1998@MacBook-Pro-Alan.local>
2018-10-01 22:24:00 +0100

    added login migration

--- src/migrations/1538422267610-login.js
+++ src/migrations/1538422267610-login.js
@@ -1,14 +1,28 @@
-
+import { ObjectId } from "mongodb";
+import { Login, LoginModel } from "../models/login";
+import { hash, genSalt } from 'bcrypt'
+import { dropCollection } from 'mongoose'
 /**
  * Make any changes you need to make to the database here
  */
-export async function up () {
-  // Write migration here
+export async function up (done) {
+  let salt = await genSalt(10);
+  let hashedPass = hash('test1234', salt)
+  let user = new Login(
+    email = 'alan@test.com',
+    password = hashedPass,
+    userId = ObjectId('') //please add stuff here Killian after creating a test user with email alan@test.com
+  )
+
+  await new LoginModel(user).save()
+  done()
 }
 
 /**
  * Make any changes that UNDO the up function side effects here (if possible)
  */
-export async function down () {
+export async function down (done) {
   // Write migration here
+  await dropCollection('logins')
+  done()
 }
